9b542160fc0a071a20580d46bbf9a423bbaf88ce
Milijana Tomic <mlgalic@gmail.com>
2020-05-23 01:05:28 -0500

    Providing function for finding locations at MapBox API, based on user's query

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,6 @@
 const APIkeyWinTransit = 'eM0x2PJKCyqqY7BOlun';
 const APIkeyMapbox = 'pk.eyJ1IjoibWlsaXRvbSIsImEiOiJja2E2YmtpN2cwNmhzMnlvejA0cm5kamtpIn0.DvZmdFkPPyBWuuW-TgOpTw';
+const bboxWinnipeg = '-97.325875, 49.766204, -96.953987, 49.99275';
 
 const ulElem = document.querySelectorAll('ul.origins, ul.destinations');
 const formElem = document.querySelectorAll('form');
@@ -16,4 +17,10 @@ formElem.forEach(element => element.addEventListener('submit', function(event) {
   event.preventDefault();
 }))
 
-
+function findLocation(query, point) {
+  fetch(`https://api.mapbox.com/geocoding/v5/mapbox.places/${query}.json?limit=10&bbox=${bboxWinnipeg}&access_token=${APIkeyMapbox}`)
+    .then(resp => resp.json())
+    .then(data => {
+      console.log(data);
+      data.features.forEach(location => displayLocations(location, point))});  
+}
