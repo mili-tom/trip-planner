241d219fbc7407973e6cf41d706eefaa2ac2571e
Milijana Tomic <mlgalic@gmail.com>
2020-05-23 02:01:58 -0500

    Taking data about longitude and latitude for selected starting and ending destinations

--- src/js/app.js
+++ src/js/app.js
@@ -19,7 +19,7 @@ formElem.forEach(element => element.addEventListener('submit', function(event) {
 
 function findLocation(query, point) {
   fetch(`https://api.mapbox.com/geocoding/v5/mapbox.places/${query}.json?limit=10&bbox=${bboxWinnipeg}&access_token=${APIkeyMapbox}`)
-    .then(resp => resp.json())
+    .then(response => response.json())
     .then(data => {
       console.log(data);
       deletePreviousSearch(point);
@@ -39,35 +39,55 @@ function deletePreviousSearch(point) {
 
 function displayLocations(location, point) {
   ulElem.forEach(element => {      
-  //element.innerHTML = '';
-  if (element.className.includes(point)) {
-    element.insertAdjacentHTML('beforeend',
-    `<li data-long=${location.center[0]} data-lat=${location.center[1]}>
-      <div class="name">${location.text}</div>
-      <div>${location.properties.address}</div>
-    </li>`
-    )
-  }
-});
+    if (element.className.includes(point)) {
+      element.insertAdjacentHTML('beforeend',
+        `<li data-long=${location.center[0]} data-lat=${location.center[1]}>
+          <div class="name">${location.text}</div>
+          <div>${location.properties.address}</div>
+        </li>`
+      )
+    }
+  });
 }
 
 ulElem.forEach(element => element.addEventListener('click', function(event) {
   const selectedLocation = event.target.closest('li');
   //console.log(selectedLocation);
   const parentElem = selectedLocation.closest('ul');
-  //let originLong, originLat, destLong, destLat;
+  let originLong, originLat, destLong, destLat;
   
   if (selectedLocation !== null) {
     const siblingsElem = getAllSiblings(selectedLocation, parentElem);
   
     selectedLocation.classList.add('selected');
     siblingsElem.forEach(ele => ele.classList.remove('selected'));
+
+    const selectedOrigLocation = document.querySelector(`.origins > .selected`);  
+    const selectedDestLocation = document.querySelector(`.destinations > .selected`);
+    //console.log(selectedOrigLocation)
+    if (selectedOrigLocation !==null && selectedDestLocation !== null) {
+    originLong = selectedOrigLocation.dataset.long;
+    //console.log(originLong);
+    originLat = selectedOrigLocation.dataset.lat;
+    //console.log(originLat);
+    destLong = selectedDestLocation.dataset.long;
+    //console.log(destLong);
+    destLat = selectedDestLocation.dataset.lat;
+    //console.log(destLat);
+    }
+  
+    if (originLong !== undefined && originLat !== undefined && destLong !== undefined && destLat !== undefined) {
+      console.log(originLong, originLat, destLong, destLat);
+      //planTrip(originLong, originLat, destLong, destLat);
+    }
   }
 }))
   
-//https://stackoverflow.com/questions/4378784/how-to-find-all-siblings-of-the-currently-selected-dom-object
+//source: https://stackoverflow.com/questions/4378784/how-to-find-all-siblings-of-the-currently-selected-dom-object
+//function takes two parameters (element and its parent) and based on that makes array of all elements that are children of selected element's parent (in this case all <li> inside of <ul>); then it filters that array and returns array of all sibling elements (just element itself is excluded from array)
 function getAllSiblings(element, parent) {
   const children = [...parent.children];
   return children.filter(child => child !== element);
 }
-  
+
+
