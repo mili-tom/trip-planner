8dfc41b27ca964d0a497896633172e0f388e03c6
Milijana Tomic <mlgalic@gmail.com>
2020-05-24 02:55:51 -0500

    Function that transpiles app.js from ES6 to ES5 and then covert it in oneline js file

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,21 @@
 const gulp = require('gulp');
 const cleanCSS = require('gulp-clean-css');
-//const babel = require('gulp-babel');
-//const uglify = require('gulp-uglify');
+const babel = require('gulp-babel');
+const uglify = require('gulp-uglify');
 
 function minifyCSS() {
   return gulp.src('src/css/*.css')
     .pipe(cleanCSS())
     .pipe(gulp.dest('dist/css'));
 }
+
+function transpileAndUglifyJS() {
+  return gulp.src('src/js/*.js')
+    .pipe(babel({
+        presets: ['@babel/env']
+    }))
+    .pipe(uglify())
+    .pipe(gulp.dest('dist/js'))
+}
+
+exports.default = transpileAndUglifyJS;
